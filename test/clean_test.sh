0a0bbcbfb65ac46761f69900e8654366e266072a
Alessandro Pandini <alessandro.pandini@gmail.com>
2016-08-23 12:49:03 +0100

    add test outcome

--- test/clean_test.sh
+++ test/clean_test.sh
@@ -5,3 +5,5 @@ rm -f T01/fixed_displacements_scores_T01.dat
 rm -f T01/fixed_force_details.h5
 rm -f T01/fixed_force_scores_T01.dat
 rm -f T01/log.T01.log
+rm -f check/T01.diff
+rm -f check/T01.out
--- test/run_test.sh
+++ test/run_test.sh
@@ -1,5 +1,13 @@
 ######################################################################
-# T01
+# T01: dfs
 cd T01
 bash T01.sh
 cd ..
+cksum T01/fixed_*_scores_T01.dat > check/T01.out
+diff check/T01.out check/T01.chk > check/T01.diff
+if [ -s check/T01.diff ]
+then
+        echo "TEST T01: FAILED"
+else
+        echo "TEST T01: PASSED"
+fi
