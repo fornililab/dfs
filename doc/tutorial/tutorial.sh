4abf2471ca8c47a9501e84ffb0cfdcc54237862e
Matteo Tiberti <matteo.tiberti@gmail.com>
2016-08-31 15:39:08 +0100

    switched tutorial to p53

--- doc/tutorial/tutorial.sh
+++ doc/tutorial/tutorial.sh
@@ -3,6 +3,10 @@
 # running the dfs script to get the rescuability matrices and then the 
 # compensatory_power script to calculate the rescaubility scores.
 
+
+#################################################################################
+# 1 - use the Dual Force Scanning method to generate rescuability scores
+
 # First we run the dfs script that will generate the rescuability
 # score matrices. We will not save any additional details file
 # for the moment as it's not strictly required for the successive
@@ -15,7 +19,12 @@ dfs=$scripts_dir"/dfs"
 compp=$scripts_dir"/compensatory_power"
 
 # structure to be used to generate the Anisotropic Network Model (ANM)
-pdb="original.pdb"
+pdb="1TSR.pdb"
+
+# selection of atoms to be used for the ANM model. We're going to use
+# CA atoms from chain A from p53's multi-chain PDB 1TSR
+
+selection="chain A and protein and name CA"
 
 # distance cut-off for ANM
 c=15.0
@@ -33,16 +42,21 @@ sm_config=config_file_secondary_mut.ini
 np=8
 
 # command line
-echo "now running: $dfs -p $pdb -r $pm_config -f $sm_config -c $c -g $g -s "name CA" --np $np"
-$dfs -p $pdb -r $pm_config -f $sm_config -c $c -g $g -s "name CA" --np $np
+echo now running: $dfs -p $pdb -r $pm_config -f $sm_config -c $c -g $g -s $selection --np $np -v
+$dfs -p $pdb -r $pm_config -f $sm_config -c $c -g $g -s $selection --np $np
 
 # Two matrix files have been saved: one that contains outcome of the calculation
-# in constant force mode and the other in constant displacement mode. We feed 
-# both of them to the compensatory_power script in order to calculate the CP index
-# on the average matrix:
+# in constant force mode and the other in constant displacement mode.
 
-echo "now running: $compp -m fixed_force_scores.dat fixed_displacements_scores.dat -p original.pdb"
-$compp -m fixed_force_scores.dat fixed_displacements_scores.dat -p original.pdb
+
+#################################################################################
+# 2 - calculate rescuability power index
+
+# We feed both matrices calculated in the previous step to the compensatory_power
+# script in order to calculate the CP index on the average matrix:
+
+echo "now running: $compp -m fixed_force_scores.dat fixed_displacements_scores.dat -p $pdb -s $selection"
+$compp -m fixed_force_scores.dat fixed_displacements_scores.dat -p $pdb -s $selection
 
 # The output file normalized_score.dat contains the per-residue Compensatory
-# power score
+# power score.
