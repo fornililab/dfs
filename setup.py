fb72927c4668b5efbf0d9d65578b46462da746b4
Matteo Tiberti <matteo.tiberti@gmail.com>
2017-08-10 09:56:58 +0100

    changed citation lines

--- setup.py
+++ setup.py
@@ -40,8 +40,6 @@ scripts = ['scripts_src/dfs',
            'scripts_src/data_muncher',
            'scripts_src/dotM']
 
-
-
 setup(
     name='DFS',
     version=__version__,
@@ -53,19 +51,16 @@ setup(
     packages=packages,
     ext_modules=cythonize(extensions),
     license='GPL v3',
-    keywords=('protein, dynamics, elastic network model, '
-              'Gaussian network model, anisotropic network model, '
+    keywords=('protein, dynamics, elastic network model, anisotropic network model, force, compensatory mutations, force scanning'
               'Protein Data Bank, PDB, ANM'),
     classifiers=[
                  'Development Status :: 5 - Production/Stable',
                  'Intended Audience :: Education',
                  'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: MIT License',
                  'Operating System :: MacOS',
                  'Operating System :: POSIX',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 3',
                  'Topic :: Scientific/Engineering :: Bio-Informatics',
                  'Topic :: Scientific/Engineering :: Chemistry',
                 ],
